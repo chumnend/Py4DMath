12da95e7845b36cda868e5ca6b990b14fd973e74
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-07-31 20:26:08 -0400

    add copy method to Vector3D

--- Py4DMath/vector.py
+++ Py4DMath/vector.py
@@ -1,3 +1,5 @@
+import copy
+
 class Vector3D:
   def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0):
     self.x = x
@@ -6,3 +8,6 @@ class Vector3D:
 
   def __repr__(self):
     return f"{self.__class__.__name__} (x={self.x} y={self.y} z={self.z})"
+
+  def copy(self):
+    return copy.deepcopy(self)
--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,13 +1,21 @@
 from Py4DMath import Vector3D
 
-def test_Vector3D_initialized_with_no_values():
+def test_Vector3D_init_with_no_values():
   v = Vector3D()
   assert v.x == 0.0
   assert v.y == 0.0
   assert v.z == 0.0
 
-def test_Vector3D_initialized_with_values():
+def test_Vector3D_init_with_values():
   v = Vector3D(1.0, 2.0, 3.0)
   assert v.x == 1.0
   assert v.y == 2.0
   assert v.z == 3.0
+
+def test_Vector3D_copy():
+  v = Vector3D(1, 2, 3)
+  copy_v = v.copy()
+
+  assert v.x == copy_v.x
+  assert v.y == copy_v.y
+  assert v.z == copy_v.z
